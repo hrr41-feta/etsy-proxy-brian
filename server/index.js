cb50ff64c87027e2702e9d6fa7823e5ae8b4a064
Brian Hambleton <bhambl3t0n1@gmail.com>
2019-11-14 17:16:14 -0800

    Changed proxy to use deployed service

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const proxy = require('http-proxy-middleware');
 const PORT = 4000;
 app = express();
 
-app.use(proxy('/products/*', {target: 'http://localhost:3003'}));
+app.use(proxy('/products/*', {target: 'http://ec2-3-17-61-156.us-east-2.compute.amazonaws.com:3003'}));
 app.use(express.static('public'));
 
 app.listen(PORT, () => {
