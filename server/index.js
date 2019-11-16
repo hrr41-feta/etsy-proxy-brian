22c5383d6778441f9c8efc149a75eb44c87bdf6d
Brian Hambleton <bhambl3t0n1@gmail.com>
2019-11-15 23:16:41 -0800

    Added loaderio get request to proxy

--- server/index.js
+++ server/index.js
@@ -5,7 +5,13 @@ const PORT = 4000;
 app = express();
 
 app.use(proxy('/products/*', {target: 'http://ec2-3-17-61-156.us-east-2.compute.amazonaws.com:3003'}));
+app.use(proxy('/api/checkout/*', {target: 'http://ec2-52-15-159-32.us-east-2.compute.amazonaws.com:1234'}));
+
 app.use(express.static('public'));
+
+app.get('/loaderio-f9dedb3ba75f0f968f2259703ca68ade', (req, res) => {
+  res.send('loaderio-f9dedb3ba75f0f968f2259703ca68ade');
+})
 
 app.listen(PORT, () => {
   console.log(`listening at port ${PORT}:`);
